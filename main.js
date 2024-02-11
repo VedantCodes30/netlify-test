15c17fd29e00bc6e99c809cab9e8c17aa7bcda92
Vedant Kale <103518903+VedantCodes30@users.noreply.github.com>
2024-02-11 14:18:20 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+const footer = document.createElement('footer')
+footer.innerHTML = `<footer>
+    <p>VedantCodes 🚀</p>
+    <ul>
+        <li>GitHub</li>
+        <li>Instagram</li>
+        <li>LinkedIn</li>
+    </ul>
+</footer>`;
+
+document.body.appendChild(footer);
