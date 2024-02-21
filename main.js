2ba67a88e6c57911b70ab695af814a77ec786180
VEDANT <vedantcodes30@gmail.com>
2024-02-21 15:04:19 +0530

    added skills section
    same as desc

--- main.js
+++ main.js
@@ -2,10 +2,11 @@ const footer = document.createElement('footer')
 footer.innerHTML = `<footer>
     <p>VedantCodes 🚀</p>
     <ul>
-        <li>GitHub</li>
-        <li>Instagram</li>
-        <li>LinkedIn</li>
+        <li><a href="https://www.github.com" target=_blank>GitHub</a></li>
+        <li><a href="https://www.instagram.com/vedantcodes" target=_blank>Instagram</a></li>
+        <li><a href="https://www.linkedin.com/in/vedant-kale-a053a424a" target=_blank>LinkedIn</a></li>
     </ul>
 </footer>`;
+
 
 document.body.appendChild(footer);
